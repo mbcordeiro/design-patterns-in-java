060cdb42696115b7831ee5e0749852558ea1b235
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-02-17 00:06:15 -0300

    Implementation without desing pattern usage

--- design-patterns/store-project/src/MainDiscount.java
+++ design-patterns/store-project/src/MainDiscount.java
@@ -1,14 +1,24 @@
 import domain.budget.Budget;
+import domain.budget.BudgetItem;
 import domain.discount.DiscountCalculator;
 
 import java.math.BigDecimal;
 
 public class MainDiscount {
     public static void main(String[] args) {
-        final var  budget = new Budget(new BigDecimal("200"), 6);
-        final var  budgetSecond = new Budget(new BigDecimal("1000"), 1);
+        final var  budget = new Budget();
+        budget.addItem(new BudgetItem(new BigDecimal("200")));
+
+        final var  budgetSecond = new Budget();
+        budgetSecond.addItem(new BudgetItem(new BigDecimal("1000")));
+
+        final var  budgetThird = new Budget();
+        budgetThird.addItem(new BudgetItem(new BigDecimal("500")));
+
         final var discountCalculator = new DiscountCalculator();
         System.out.println(discountCalculator.calculate(budget));
         System.out.println(discountCalculator.calculate(budgetSecond));
+        System.out.println(discountCalculator.calculate(budgetThird));
+        System.out.println(discountCalculator.calculate(budgetThird));
     }
 }
